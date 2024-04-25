e33422a0bb2f3bcbe19142670fe4684d6da98ac2
RGV <ricardo.guzman.velasco@outlook.com>
2024-04-25 13:54:57 +0200

    feat: earn one cookie at time when needed

---
+++ Assets/Cookies/Runtime/Application/EarnCookies.cs
@@ -0,0 +1,13 @@
+﻿namespace Cookies.Runtime.Application
+{
+    public class EarnCookies
+    {
+        const int CookiesPerTime = 1;
+        readonly CookieBox domain = new CookieBox();
+
+        public void Run()
+        {
+            
+        }
+    }
+}
