26014c61d8b4d3a69014dd62576b0b2c788a4a27
RGV <ricardo.guzman.velasco@outlook.com>
2024-05-01 14:14:26 +0200

    refactor: remove state duplication

--- Assets/Cookies/Runtime/Domain/PowerUp.cs
+++ Assets/Cookies/Runtime/Domain/PowerUp.cs
@@ -1,12 +1,11 @@
 
 public class PowerUp
 {
-    public int Price { get; private set; } = 5;
+    public int Price => 5 * Level;
     public int Level { get; private set; } = 1;
 
     public void AddLevel()
     {
         Level++;
-        Price = 5 * Level;
     }
 }
